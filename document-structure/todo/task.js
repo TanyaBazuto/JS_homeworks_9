c1bcb8b78f6439f596ab9db953cff6be2f714a49
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2025-02-15 18:55:10 +0300

    Update task.js

---
+++ document-structure/todo/task.js
@@ -0,0 +1,33 @@
+let listTask = document.getElementById('tasks__list');
+let inputTask = document.getElementById('task__input');
+let addTask = document.getElementById('tasks__add');
+
+
+function addNewTask(e) {
+    if (inputTask.value != '') {
+        listTask.insertAdjacentHTML('afterBegin', 
+            `<div class="task">
+                <div class="task__title">
+                ${inputTask.value}
+                </div>
+                <a href="#" class="task__remove">&times;</a>
+            </div>`);
+        inputTask.value = '';
+        e.preventDefault();
+
+        let remove = document.querySelector('.task__remove');
+        let task = document.querySelector('.task');
+    
+        
+        remove.addEventListener('click', function(e) {
+            e.preventDefault();
+            task.remove();
+        });
+
+        return false;
+    } else {
+        e.preventDefault();
+    } 
+}
+
+addTask.addEventListener('click', addNewTask);
