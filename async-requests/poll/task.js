60ce3c89f0105b4b42d6f9d76862902ba1e5ebd5
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2025-02-20 12:57:12 +0300

    Update task.js

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,24 +1,28 @@
-const pollAnswers = document.getElementById('poll__answers')
-pollQuestion = document.getElementById('poll__title')
-xhr = new XMLHttpRequest()
+const pollAnswers = document.getElementById('poll__answers');
+const pollQuestion = document.getElementById('poll__title');
+const xhr = new XMLHttpRequest();
 
-xhr.addEventListener('readystatechange', () => {
-  if (xhr.readyState == xhr.DONE) {
-    const data = JSON.parse(xhr.responseText).data
-    pollQuestion.innerText = data.title
+xhr.onreadystatechange = () => {
+  if (xhr.readyState !== 4) {
+    return;
+  } else if (xhr.readyState == 4 && xhr.status == 200) {
+    const data = JSON.parse(xhr.responseText).data;
+    pollQuestion.innerText = data.title;
 
     for (const answer of data.answers) {
-      const button = `<button class='poll__answer'>${answer}</button>`
-      pollAnswers.insertAdjacentHTML('beforeEnd', button)
+      const button = `<button class='poll__answer'>${answer}</button>`;
+      pollAnswers.insertAdjacentHTML('beforeEnd', button);
     }
 
-    const pollButtons = document.querySelectorAll('.poll__answer')
+    const pollButtons = document.querySelectorAll('.poll__answer');
     for (const pollButton of pollButtons) {
       pollButton.addEventListener('click', () => {
-        alert('Спасибо, ваш голос засчитан!')
-      })
+        alert('Спасибо, ваш голос засчитан!');
+      });
     }
+  } else {
+    alert(`Ошибка! Код ответа: ${xhr.status}.`);
   }
-})
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll')
-xhr.send()
+};
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+xhr.send();
